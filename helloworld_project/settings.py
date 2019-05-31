f3abc21537b6c400e6e011b5ac220da54286a977
Karl Zakhary <karlmaged33@gmail.com>
2019-05-31 22:13:10 +0200

    adding allowed host

--- helloworld_project/settings.py
+++ helloworld_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ema&(=fwyo25$5zk23^@u^4$$)=pebglxp-g2^q7_&e^hfxr5s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
